c50a1c2ca5de7261861c1df82e03d943e577afbf
Beth Day <bethmarsh1018@gmail.com>
2023-02-25 15:53:22 -0500

    Fixed syntax to mirror test.

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -12,13 +12,13 @@ public class BestLoanRates {
         String name = scanner.nextLine();
         System.out.println("Hello " + name);
 
-        System.out.println("Enter the loan term (in years) ");
+        System.out.println("Enter the loan term (in years)");
         int loanTermInYears = scanner.nextInt();
         float bestRate = getRates(loanTermInYears);
         if(bestRate == 0.0f){
-            System.out.println("No available rates for term: " + loanTermInYears);
+            System.out.println("No available rates for term: " + loanTermInYears + " years");
         } else {
-            System.out.println("Best available rate: " + getRates(loanTermInYears) + "%");
+            System.out.println("Best Available Rate: " + getRates(loanTermInYears) + "%");
         }
         scanner.close();
     }

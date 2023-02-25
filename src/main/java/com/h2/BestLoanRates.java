c973dc4d42bf6fd21ddaf99c880f085512582ac3
Beth Day <bethmarsh1018@gmail.com>
2023-02-25 15:47:47 -0500

    updated return value in getRates

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -8,7 +8,7 @@ public class BestLoanRates {
     public static final Map<Integer, Float> bestRates = Map.of(1, 5.50f, 2, 3.45f, 3, 2.67f);
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your name: ");
+        System.out.println("Enter your name");
         String name = scanner.nextLine();
         System.out.println("Hello, " + name);
 
@@ -25,7 +25,7 @@ public class BestLoanRates {
 
     public static float getRates(int loanTermInYears){
         if(bestRates.containsKey(loanTermInYears)){
-            return loanTermInYears;
+            return bestRates.get(loanTermInYears);
         }
         return 0.0f;
     }
